1bab636d4a4c02dd3a8e014c3be3a673019dbc73
zohabiya123 <zohabiyanaeem@gmail.com>
2026-02-18 17:15:20 +0500

    Add Sanity env variables and client setup

--- src/sanity/lib/sanity.js
+++ src/sanity/lib/sanity.js
@@ -1,10 +1,10 @@
 import { createClient } from '@sanity/client'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // ← Yahan Project ID use hoti hai
-  dataset:  process.env.NEXT_PUBLIC_SANITY_DATASET, 
+  projectId: process.env.uku5j8l5, // ← Yahan Project ID use hoti hai
+  dataset:  process.env.production, 
   useCdn: true,
   apiVersion: '2025-01-13',
-  token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN
+  token: process.env.ski5FSNMcX6Aq4ItavsKSXcR3vTonxxk9FQE1zoni5t4tUtSTnZJ2ryajozlmG177La3wGr9rU2p3HmW1Kvrm97DqBBFAHxdVBftHG2wjMCjuAY9i8RcAZYWQTjIbQ0BP8XcX2N8xD79ZmoZoH6ZKJYG5pdntlfbeGRQtmF4S9A0EkAz8GNn
 })
 export default client;
